52a8e7fb424bb787cdde2c40db4ab09dfe7333fb
Srinu Kusumanchi <srinukusumanchi@gmail.com>
2026-01-08 08:25:03 -0500

    Add Allure commandline support and update Playwright configuration for enhanced reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html"], ["line"], ["allure-playwright"]],
   timeout: 30000,
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -32,7 +32,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
+    screenshot: 'on',
+    video: 'on'
 
   },
   expect: {
